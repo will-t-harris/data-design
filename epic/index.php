1d9eb9daeb202a6a030a00ed68334b85348ce0d4
Will Harris <will.t.harris@protonmail.com>
2019-07-09 15:26:56 -0600

    add persona for power-user patty

--- epic/index.php
+++ epic/index.php
@@ -5,6 +5,7 @@
 	</head>
 	<body>
 		<h1>Phase 1 Index</h1>
+		<p>For the data design project, I am going to use Craigslist as my site</p>
 		<a href="pages/persona.php">Persona</a>
 		<a href="pages/user-story.php">User Story</a>
 		<a href="pages/use-case.php">Use Case/Interaction Flow</a>
