f271a9a89a2cba5e8784d6d46799ceea91d53dd7
Will Harris <will.t.harris@protonmail.com>
2019-07-09 15:12:40 -0600

    add php file for each page in /pages directory

---
+++ epic/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Phase 1 Index</title>
+	</head>
+	<body>
+		<h1>Phase 1 Index</h1>
+		<a href="pages/persona.php">Persona</a>
+		<a href="pages/user-story.php">User Story</a>
+		<a href="pages/use-case.php">Use Case/Interaction Flow</a>
+		<a href="pages/conceptual-model.php">Conceptual Model</a>
+	</body>
+</html>
