d5d187608bd5da114266c197a1d3f96c421bbc5f
Will Harris <will.t.harris@protonmail.com>
2019-07-09 15:33:48 -0600

    add pre and post condition for use-case for patty

--- epic/pages/use-case.php
+++ epic/pages/use-case.php
@@ -1,1 +1,8 @@
-<h1>Test</h1>
+<h1>Use Case for Patty</h1>
+
+<p>Patty has filled a property vacancy, and needs to take down her ad.</p>
+
+<p>precondition: Patty is logged into her Craigslist account.</p>
+
+<p>postcondition: Patty has taken down her ad.</p>
+
--- epic/pages/user-story.php
+++ epic/pages/user-story.php
@@ -1,1 +1,3 @@
+<h1>User Story for Patty</h1>
+
 <p>As a power user I want to post a property for rent to the housing section</p>
