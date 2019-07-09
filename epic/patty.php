bcc307f6ca9b4ce998b93045b00509412e400614
Will Harris <will.t.harris@protonmail.com>
2019-07-09 16:04:33 -0600

    add correct html structure to patty.php, link style.css to patty.php

--- epic/patty.php
+++ epic/patty.php
@@ -1,12 +1,21 @@
-<h1>Persona</h1>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Patty's Persona</title>
+		<link rel="stylesheet" href="styles/style.css" type="text/css">
+	</head>
+	<body>
+		<h1>Persona</h1>
 
-<ul>
-	<li>The users name is <b>Patty</b>.  She would be a <b>power user</b> on Craigslist.</li>
-	<li>Female, mid-50's</li>
-	<li>Patty is a rental property owner</li>
-	<li>Patty has a very basic understanding of the internet, loosely knows how to upload pictures to websites, how to type up property ads, how to correspond via email.</li>
-	<li>Mostly accesses the website via desktop computer, though light mobile use as well.</li>
-	<li>Skeptical of technology, but recognizes that she needs to use it to find tenants in the 21<sup>st</sup> century</li>
-	<li>She uses the site solely to list properties for rent and to find potential tenants.  This is the need the application aims to fill: she needs tenants for her properties, Craigslist can put her in touch with potential tenants.</li>
-	<li>She chooses to use Craigslist because of the wide user-base, because of it's visual simplicity, and because she can easily understand how to make listings.</li>
-</ul>
+		<ul>
+			<li>The users name is <b>Patty</b>.  She would be a <b>power user</b> on Craigslist.</li>
+			<li>Female, mid-50's</li>
+			<li>Patty is a rental property owner</li>
+			<li>Patty has a very basic understanding of the internet, loosely knows how to upload pictures to websites, how to type up property ads, how to correspond via email.</li>
+			<li>Mostly accesses the website via desktop computer, though light mobile use as well.</li>
+			<li>Skeptical of technology, but recognizes that she needs to use it to find tenants in the 21<sup>st</sup> century</li>
+			<li>She uses the site solely to list properties for rent and to find potential tenants.  This is the need the application aims to fill: she needs tenants for her properties, Craigslist can put her in touch with potential tenants.</li>
+			<li>She chooses to use Craigslist because of the wide user-base, because of it's visual simplicity, and because she can easily understand how to make listings.</li>
+		</ul>
+	</body>
+</html>
