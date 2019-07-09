a2ee91b4eabad77663ad60cbdf389d5ef7fd7f77
Will Harris <will.t.harris@protonmail.com>
2019-07-09 16:05:43 -0600

    add correct html structure to use-case.php, link style.css to use-case.php

--- epic/use-case.php
+++ epic/use-case.php
@@ -1,20 +1,29 @@
-<h1>Use Case, User Stories, and interaction flow for Patty</h1>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Patty's Persona</title>
+		<link rel="stylesheet" href="styles/style.css" type="text/css">
+	</head>
+	<body>
+		<h1>Use Case, User Stories, and interaction flow for Patty</h1>
 
-<h2>User Story</h2>
+		<h2>User Story</h2>
 
-<p>As a power user I want to post a property for rent to the housing section</p>
+		<p>As a power user I want to post a property for rent to the housing section</p>
 
-<h2>Use Case</h2>
+		<h2>Use Case</h2>
 
-<p>Patty has filled a property vacancy, and needs to take down her ad.</p>
+		<p>Patty has filled a property vacancy, and needs to take down her ad.</p>
 
-<p>precondition: Patty has just logged into her Craigslist account.</p>
+		<p>precondition: Patty has just logged into her Craigslist account.</p>
 
-<p>postcondition: Patty has taken down her ad.</p>
+		<p>postcondition: Patty has taken down her ad.</p>
 
-<ul>
-	<li>Patty clicks on the active section in the 'find your postings' section viewed on logging into Craigslist</li>
-	<li>Site brings Patty to a table view listing all of her ads.</li>
-	<li>Under the 'manage' column, Patty selects delete</li>
-	<li>Her listing has been deleted</li>
-</ul>
+		<ul>
+			<li>Patty clicks on the active section in the 'find your postings' section viewed on logging into Craigslist</li>
+			<li>Site brings Patty to a table view listing all of her ads.</li>
+			<li>Under the 'manage' column, Patty selects delete</li>
+			<li>Her listing has been deleted</li>
+		</ul>
+	</body>
+</html>
