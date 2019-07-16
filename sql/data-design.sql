03295b2fa453f33849af946d69ba8ec75f4091ac
Will Harris <will.t.harris@protonmail.com>
2019-07-16 13:39:44 -0600

    add partial create statement for post table

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -11,11 +11,26 @@ CREATE TABLE user (
    userLocation CHAR(20),
    userEmail VARCHAR(128) NOT NULL,
    userPhoneNumber VARCHAR(32),
-   -- these prevent duplicate data
+   -- unique keys -- these prevent duplicate data
    UNIQUE(userEmail),
    UNIQUE(userPhoneNumber),
    -- primary key for user entity
-   PRIMARY KEY(userId)
+	PRIMARY KEY(userId)
 );
 
 -- CREATE TABLE statement for post entity
+CREATE TABLE post (
+	postId BINARY(16) NOT NULL,
+	postUserId BINARY(16) NOT NULL,
+	postPostStatusId BINARY(16) NOT NULL,
+	postArea VARCHAR(?) NOT NULL,
+	postTitle VARCHAR(70) NOT NULL,
+	postCategory VARCHAR(?),
+	postDate DATETIME(17) NOT NULL,
+	postExpiration DATETIME(17) NOT NULL,
+	postContent VARCHAR(65539) NOT NULL,
+	-- unique keys for post entity
+	UNIQUE(postUserId),
+	UNIQUE(postPostStatusId),
+	-- foreign keys for post entity
+);
